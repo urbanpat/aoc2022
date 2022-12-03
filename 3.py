b642b8b9599c07e8876509a7fb5fd1cf274b36a9
urbanpat <68732290+urbanpat@users.noreply.github.com>
2022-12-03 09:35:29 +0100

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,10 @@
+import string
+from pathlib import Path
+
+rucksacks = (Path(__file__).parent / "input").read_text().split("\n")[:-1]
+common_items = [list(set(r[len(r)//2:]).intersection(set(r[:len(r)//2])))[0] for r in rucksacks]
+print(sum([(string.ascii_lowercase + string.ascii_lowercase.upper()).index(item) + 1 for item in common_items]))
+
+groups = list(zip(*(rucksacks[::3], rucksacks[1::3], rucksacks[2::3])))
+common_items_in_groups = [list(set(e1).intersection(set(e2)).intersection(set(e3)))[0] for e1, e2, e3 in groups]
+print(sum([(string.ascii_lowercase + string.ascii_lowercase.upper()).index(item) + 1 for item in common_items_in_groups]))
